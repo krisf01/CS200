3b76c5921b1b0c36a8021c4911dafc372d81d947
Kristina Fout <64440239+krisf01@users.noreply.github.com>
2021-11-04 17:33:03 -0700

    ElevatorsV.2.1

---
+++ Floor.h
@@ -0,0 +1,48 @@
+// Programmer: Kristina Fout
+// Programmer ID: 1866884
+
+#ifndef Floor_H
+#define Floor_H
+
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+
+#include "Panel.h"
+#include "Rider.h"
+
+class Floor
+{
+    queue<Rider> upRiders, downRiders;
+  
+    public:
+    Rider removeUpRider();
+    Rider removeDownRider();
+    
+    Floor(const int, const char* const, const char* const); // elevation (inches above ground) of floor, name and label
+    operator int() const {return elevation;}
+
+    static const char* const UP;
+    static const char* const DOWN;
+    static int TOP, BOTTOM;
+    
+    bool hasUpRider( ) const {return !upRiders.empty() ? true : false;}
+    bool hasDownRider( ) const {return !downRiders.empty() ? true : false;}
+    void addRider(const Rider&);
+    
+    Panel panel;
+    const string name; // name of floor, for example: Mezzanine
+    const string label; // as it appears on the button panel
+    const int elevation; // inches above ground level
+    
+   
+    // for external reporting
+    friend ostream& operator<<(ostream&, const Floor&); // say name, location, #'s of up/down riders waiting
+    
+    int getNumberOfUpRiders() const {return upRiders.size();}
+    unsigned int getNumberOfDownRiders() const {return downRiders.size();}
+    
+};
+
+#endif
