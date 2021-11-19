a25e1f8d3293ca091c36b7052e1f9d4a82d64eec
Kristina Fout <64440239+krisf01@users.noreply.github.com>
2021-11-19 11:26:47 -0800

    Create Building.h

---
+++ ElevatorsV.3/Building.h
@@ -0,0 +1,31 @@
+// Programmer: Kristina Fout
+// Programmer ID: 1866884
+
+#ifndef Building_H
+#define Building_H
+
+#include "Floor.h"
+#include "Elevator.h"
+
+struct Building
+{
+    static Floor floors[];
+    static Elevator elevators[];
+    static const int FLOORS;
+    static const int ELEVATORS;
+
+    // helper functions
+    static void getDifferentInts(int&, int&);
+    static int getPoisson(double);
+    static void placeNewRiders(double);
+  
+    // actions
+    static void action(double); // the main "action function"
+    static bool openDoorToDisembarkRider(int); // step 1 of 9
+    static bool disembarkRider(int);
+    static bool boardRider(int);
+    static bool waitingForMoreRiders(int);
+    static bool doneWaitingMove(int);
+};
+
+#endif
